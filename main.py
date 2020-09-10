0618f7a54e95f8c0a7a34ac2409c86eb145fe30f
Retrocky <44643022+Retrocky@users.noreply.github.com>
2020-09-10 11:57:53 +0100

    Importing the CSV data.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-#importing packages
+# importing packages
 import pulp
-import pandas
+import pandas as pd
+
+# formatting csv data for the parent timings into a manipulable form with pandas
+parent_timings_csv = 'parents_timings.csv'
+parent_timings = pd.read_csv(parent_timings_csv)
