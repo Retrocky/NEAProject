184c93ff65e23b63f9df02beeb3b70543bba1752
Retrocky <44643022+Retrocky@users.noreply.github.com>
2020-09-10 12:08:38 +0100

    Small update for decision variables.

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ import pandas as pd
 # formatting csv data for the parent timings into a manipulable form with pandas
 parent_timings_csv = 'parents_timings.csv'
 parent_timings = pd.read_csv(parent_timings_csv)
+
+# setting up optimisation problem - registered as a maximisation problem
+problem = pulp.LpProblem('ParentsEvening',sense=LpMaximize)
+
+# setting up decision variables
+decision_variables = []
