3abd4d0cb631c3e499b87d40a03c2551ea735c01
Retrocky <44643022+Retrocky@users.noreply.github.com>
2020-09-16 09:33:58 +0100

    Cleaned up and sorted out some bugs

--- test.py
+++ test.py
@@ -6,22 +6,33 @@ EndTIme = 8.5
 TotalSlots = int((EndTIme - StartTime)*12)
 print(TotalSlots)
 
+# Outputs slots
+def outputSlots():
+    print('='*100)
+    for item in slots:
+        if 'Slot :' in item:
+            print("")
+            print(item)
+        else:
+            print(item)
+    print('='*100)
+
+# Creates slots
 def createSlot(teacher,student):
     slots.append((teacher+" : "+student))
 
 for i in range(TotalSlots):
-    slots.append('NEW SLOT')
+    slots.append('Slot :'+str(i))
     for teacher in teacherlist:
         verified = False
         while verified == False:
             for student in Students.keys():
                 if teacher in Students[student]:
-                    print('VERIFIED')
+                    print('Verified slot')
                     createSlot(teacher,student)
                     verified = True
 
-print(slots)
-
+outputSlots()
 print(Students.values())
 print(Students.keys())
 print(Students['Will'])
